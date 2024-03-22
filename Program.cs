46e4bdb258e0a7e1fd6c1bee306c5ca0e24ffdf0
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-03-22 17:29:02 -0300

    exercicio pronto

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Console.Writeline("pedro".ToUpper().Replace("e" , "&"));
+
+Console.Clear();
+Console.WriteLine("--- tradutor de baleês ---\n");
+Console.WriteLine("digite o texto a ser traduzido: ");
+string texto = Console.ReadLine()!;
+Console.WriteLine("\nA dory diria...\n");
+
+Console.WriteLine(texto
+.Replace("a", "aaa")
+.Replace("e", "eee")
+.Replace("i", "iii")
+.Replace("o", "ooo")
+.Replace("u", "uuu")
+.Replace("A", "AAA") 
+.Replace("E", "EEE")
+.Replace("I", "III")
+.Replace("O", "OOO")
+.Replace("U", "UUU")
+);
+Console.WriteLine();
